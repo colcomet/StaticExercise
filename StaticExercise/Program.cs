8f8a6cec73d10fd8cc8fa7bceb6ca4bf7b8f94b2
Andrew Barber <drewbarber72@gmail.com>
2022-06-17 01:08:54 -0400

    initial commit

---
+++ StaticExercise/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StaticExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"46f = {TempConverter.FahrenheitToCelsius(46):0.0}c");
+            Console.WriteLine($"102f = {TempConverter.FahrenheitToCelsius(102):0.0}c");
+            Console.WriteLine($"4f = {TempConverter.FahrenheitToCelsius(4):0.0}c");
+
+            Console.WriteLine($"38c = {TempConverter.CelsiusToFahrenheit(38):0.0}f");
+            Console.WriteLine($"-12c = {TempConverter.CelsiusToFahrenheit(-12):0.0}f");
+            Console.WriteLine($"17c = {TempConverter.CelsiusToFahrenheit(17):0.0}f");
+        }
+    }
+}
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticExercise
+{
+    internal static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double fahrenheit)
+        {
+            return (fahrenheit - 32.00) * (5.00 / 9.00);
+        }
+
+        public static double CelsiusToFahrenheit(double celsius)
+        {
+            return (celsius * 9.00 / 5.00) + 32.00;
+        }
+    }
+}
